3ff6873986034e294dc6c967399b94f4556adeec
sforsanjnarao <sanjnarao26@gmail.com>
2025-05-06 09:39:30 +0530

    initialize React app with Vite setup and basic Todo component

---
+++ todo/src/component/Todo.jsx
@@ -0,0 +1,41 @@
+ import React ,{useState} from 'react'
+ 
+ function Todo() {
+    const [task, setTask] = useState('')
+    const [list, setList]=useState([])
+    
+    const handelChange=(e)=>{
+        e.preventDefault()
+        setList([...list, task])
+        setTask("")
+    }
+    const handelDelete=(id)=>{
+        const update=[...list]
+        update.splice(id,1)
+        setList(update);
+    }
+
+   return (
+     <div>
+        <form action="" onSubmit={handelChange}>
+            <h1>Write the task</h1>
+            <input type="text" placeholder="Enter your task here" maxLength={50} value={task} onChange={(e)=>setTask(e.target.value)}/>
+            <button type='submit'>done</button>
+        </form>
+        
+
+        <div>
+            {list.map((e,index)=>(
+                <div key={index}>
+                  <p>{e}</p>
+                  <button onClick={()=>handelDelete(index)}>del</button>
+                </div>
+                
+                
+            ))}
+        </div>
+     </div>
+   )
+ }
+ 
+ export default Todo
